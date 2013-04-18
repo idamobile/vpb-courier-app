c2a4a9f5e1657b0d6eab1f883c060380417f1a05
shaubert <shaubert.alexander@gmail.com>
2013-04-18 10:18:53 +0400

    changed url for image upload

--- src/main/com/idamobile/vpb/courier/network/core/Hosts.java
+++ src/main/com/idamobile/vpb/courier/network/core/Hosts.java
@@ -14,7 +14,7 @@ public class Hosts {
 
     public static final String ACTIVATE_CARD_URL = addHost("courier/api/activatecard");
 
-    public static final String UPLOAD_IMAGE_URL_FORMAT = addHost("courier/api/uploadimage/%d/%d/%d");
+    public static final String UPLOAD_IMAGE_URL_FORMAT = addHost("courier/api/uploadimage/courier_id/%d/request_id/%d/type_id/%d");
 
     public static String getUploadImageUrl(int courierId, int orderId, int imageTypeId) {
         return String.format(UPLOAD_IMAGE_URL_FORMAT, courierId, orderId, imageTypeId);
